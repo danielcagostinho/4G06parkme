4bea923cc2282f74ce4c905c5bf8de4696f77429
Anthony Chang <chang_a96@hotmail.com>
2019-02-02 22:00:06 -0500

    added Parking Spot Activity

---
+++ App/app/src/main/java/com/example/parkinglot/ParkingSpotActivity.java
@@ -0,0 +1,13 @@
+package com.example.parkinglot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParkingSpotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parking_spot);
+    }
+}
